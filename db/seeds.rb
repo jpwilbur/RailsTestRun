2b9edf0a168523eee59cc5a9d053b2c43d6c12c8
Jarrod Wilbur <jpwilbur@hotmail.com>
2017-08-18 13:34:18 -0600

    seed to autopopulate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Stuff.create(name: 'Hat', image: 'app/assets/images/hat.jpg', description: 'you wear this on your head. It makes you look like a douche sometimes though, so be very careful')
+Stuff.create(name: 'Barn', image: 'app/assets/images/barn.jpg', description: 'Never been to one. Probably have seen one. They hold stuff. I''m sure it is much more complicated than that.')
+Stuff.create(name: 'Beach', image: 'app/assets/images/beach.jpg', description: 'I love the beach. I mean, who wouldn''t really? You can go in the water, get a tan, and enjoy the grimpy, salty feeling when you get home')
+Stuff.create(name: 'Chicken', image: 'app/assets/images/chicken.jpg', description: 'They lay eggs. Theys about all I got for you')
+Stuff.create(name: 'Church', image: 'app/assets/images/church.jpg', description: 'These places are the best. I shouldn''t say more since I probably have a bias.')
+Stuff.create(name: 'Cow', image: 'app/assets/images/cow.jpg', description: 'They give us milk and meat. What more could you want from one creature?')
+Stuff.create(name: 'Meme', image: 'app/assets/images/meme.jpg', description: 'The thing that changed the internet forever. They make you laugh, and they make you angry. And we love them.')
+Stuff.create(name: 'Mountian', image: 'app/assets/images/mountain.jpg', description: 'We climb them. We marvel them. And they never change to us.')
