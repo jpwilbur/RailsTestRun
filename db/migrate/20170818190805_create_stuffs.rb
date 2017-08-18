d9ac6fe229acfe51f219feb00074835ebfa83b19
Jarrod Wilbur <jpwilbur@hotmail.com>
2017-08-18 13:09:44 -0600

    added model params

--- db/migrate/20170818190805_create_stuffs.rb
+++ db/migrate/20170818190805_create_stuffs.rb
@@ -1,7 +1,9 @@
 class CreateStuffs < ActiveRecord::Migration[5.0]
   def change
     create_table :stuffs do |t|
-
+      t.string :name
+      t.string :image
+      t.string :description
       t.timestamps
     end
   end
