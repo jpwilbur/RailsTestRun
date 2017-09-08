718fdb6dd2f97aff3935a0574584a12af388c793
Jarrod Wilbur <jpwilbur@hotmail.com>
2017-09-07 20:35:30 -0600

    direct edit requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/stuff' => 'stuff#index'
   get '/stuff/new' => 'stuff#new', as: :new
+  get '/stuff/edit/:id' => 'stuff#edit', as: :edit
+  get '/stuff/delete/:id' => 'stuff#delete', as: :delete
 end
