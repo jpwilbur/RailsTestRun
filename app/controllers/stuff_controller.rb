fd2868a227d045c2b931ac995f5e3543d3a50300
Jarrod Wilbur <jpwilbur@hotmail.com>
2017-09-07 20:36:06 -0600

    logic for edit and delete

--- app/controllers/stuff_controller.rb
+++ app/controllers/stuff_controller.rb
@@ -2,4 +2,28 @@ class StuffController < ApplicationController
   def index
     @stuff = Stuff.all
   end
+
+  def edit
+    @stuff = Stuff.find(params[:id])
+      if @stuff.update(stuff_params)
+        redirect_to @stuff
+      else
+        render "edit"
+      end
+  end
+
+  def delete
+    Stuff.find(params[:id]).destroy
+    @stuff = Stuff.all
+    render "index"
+  end
+
+  def new
+
+  end
+
+  private
+    def stuff_params
+      params.require(:stuff).permit(:name, :description)
+    end
 end
